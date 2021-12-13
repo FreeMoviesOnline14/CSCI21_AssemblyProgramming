aee589d2d2b0c169212ef9aad38d01595704471d
Marcellus Von Sacramento <mvps815165@gmail.com>
2021-12-12 18:33:08 -0800

    Added switch case

--- q5.asm
+++ q5.asm
@@ -15,12 +15,15 @@
 #
 #
 
-    .text
-    .globl main
+        .data
+jTable: .word   hundredsPlace, tensPlace, onesPlace  # jumptable
+
+        .text
+        .globl main
 
 main:                       # start of text segment
     li      $t1, 0          # counter for the digit place
-
+    la      $t3, jTable     # load address of the jumptable
     lui     $t0, 0xffff     # 0xFFFF0000, base address of I/O
 
 waitLoop:
@@ -33,11 +36,25 @@ waitLoop:
 # extract the input from receiver data register
     lw      $s1, 4($t0)     # load contents of receiver data register
     nop                     # delay
-    sll     $s1, $s1, 28         # shift left logical by 28 to get the lower 4 bits
-    srl     $s1, $s1, 28         # shift right logical to get actual decimal digit
+    sll     $s1, $s1, 28    # shift left logical by 28 to get the lower 4 bits
+    srl     $s1, $s1, 28    # shift right logical to get actual decimal digit
 
 # check which digit place was read (i.e. 1st digit will be on the "hundreds" place, 2nd will be on the "tens" place, and so on)
+    sll     $s0, $t1, 2     # calculate offset from base address of jumptable
+    addu    $s0, $s0, $t3   # calculate jump address
+    lw      $s2, 0($s0)     # load the address contained in $s0
+    nop                     # delay
+    jr      $s2             # jump to address contained in $s0
+    nop  
 
+hundredsPlace:
+    li      $s0, 100        # $s0 = 100
+    mult    $s1, $s0        # calculate the "hundreds" place by multiplying input by 100
+    mflo    $s1             # get result from lo register
+    #j       display        # jump back to waitloop to get next input
+  #  nop
+
+display:
     or      $a0, $s1, $s1   # copy contents of $s1 to $a0 for display
     li      $v0, 1          # service code for printing string
     syscall
