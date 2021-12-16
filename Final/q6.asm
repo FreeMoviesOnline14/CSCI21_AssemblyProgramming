5d9462f96f9ff0b1e30fbb1313d86112ca5f86cc
Marcellus Von Sacramento <mvps815165@gmail.com>
2021-12-16 12:41:55 -0800

    Update source throughout

--- Final/q6.asm
+++ Final/q6.asm
@@ -6,27 +6,28 @@
 # Author(s): Marcellus Von Sacramento
 #
 # Purpose: The purpose of this program is to take the user character and print it in a console until 'q' is typed.
-#		   When 'q' is type, program will terminate.
+#          When 'q' is type, program will terminate.
 #
 # Copyright(c)
 #
 #
-# Last date modified: 12/15/2021
+# Last date modified: 12/16/2021
 #
 #
 
-        .data
-initMsg: .asciiz "Pressed any key... Press 'q' to Exit...\n\n"
-extMsg: .asciiz "Exiting program...\n"
+                .data
+initMsg:        .asciiz "Pressed any key... Press 'q' to Exit...\n\n"
+extMsg:         .asciiz "\nExiting program...\n"
 
-        .text
-        .globl main
+                .text
+                .globl main
 
 main:   # start of text segment
 
-        la      $a0, initMsg
-        li      $v0, 4  
+        la      $a0, initMsg        # load address of initial message to be printed
+        li      $v0, 4              # service code for printing string
         syscall 
+
 # enable all interrupts for status register $12 and receiver control register
         li      $s0, 0xffff0000     # base address of I/O
         li      $s1, 2              # 0x000000002 to enable keyboard interrupt
@@ -34,23 +35,34 @@ main:   # start of text segment
         li      $s1, 0xfff1         # will be used to enable all exception in status register $12
         mtc0    $s1, $12
 
-here:
-        j       here    # stay here until q is pressed, kernel will handle that
+stayHere:
+        j       stayHere                # stay here until q is pressed, kernel will handle that
 
-        li      $v0, 10     # service code for returning control to OS
+exit:
+        la      $a0, extMsg     # load address of exit message
+        li      $v0, 4          # service code for printing text
+        syscall
+        li      $v0, 10         # service code for returning control to OS
         syscall
 
 
-        .kdata
-        .align 2
-save1:  .word 0
-save2:  .word 0
+###########################################################
+#                                                         #
+#                Exception Handler Code                   #
+#                                                         #
+###########################################################
 
-entryMsg: .asciiz "Entered exception handler... Printing pressed key...\n"
-out:      .asciiz "You pressed: "
-ignore: .asciiz "Exception level greater than 0... Ignoring exception...\n"
+                .kdata
+                .align 2
+save1:          .word 0
+save2:          .word 0
+entryMsg:       .asciiz "Entered exception handler... Printing pressed key...\n"
+out:            .asciiz "\nYou pressed: "
+ignoreMsg:      .asciiz "Exception level greater than 0... Ignoring exception...\n"
+lf:              .asciiz "\n"
         
-        .ktext  0x80000180 
+
+                .ktext  0x80000180 # address of kerne text segment
 
 exceptionHandler:       # start of kernel text segment
         sw      $v0, save1      # save $v0 to save1
@@ -73,17 +85,21 @@ exceptionHandler:       # start of kernel text segment
                                 # if exception code is not 0 then we ignore the exception
         li      $a1, 0xffff0000 # load base address of I/O
         lw      $v1, 4($a1)     # extract input from receiver data register, that was placed here from main
-        nop
-        beq     $k0, $v1, qWasPressed   # if 'q' was pressed then we terminate the program
-        nop
         la      $a0, out        # load address of output message
         li      $v0, 4          # service code for printing string
         syscall
 
         sw      $v1, 12($a1)    # display pressed key to console
+        la      $a0, lf         # load address of new line to be printed
+        li      $v0, 4          # service code for printing string
+        syscall
+
+# check if 'q' was pressed
+        beq     $k0, $v1, qWasPressed   # if 'q' was pressed then we terminate the program
+        nop
+
         j       done            # exit exception handler
         
-
 # if exception level greater than 0, we ignore exception
 # print message
 ignore:
@@ -93,21 +109,16 @@ ignore:
         j       done
         nop
 
-
 # exit program when 'q' was pressed
 qWasPressed:
-        lw      $k1, $14        # load address where exception occurred
-        nop
+        mfc0    $k1, $14        # load address where exception occurred
         addiu   $k1, 4          # add 4 to EPC because we are in branch/jump when returned to main
-        sw      $k1 , $14       # store next address of the instruction to execute when returned to when
-
+        mtc0    $k1, $14        # store next address of the instruction to execute when returned to when
 
 done:
 # restore original status before exception handling phase
         lw      $v0, save1      # restore $v0 old value
         lw      $a0, save2      # restore $a0, old value
-
-
         mtc0    $zero, $13      # clear cause register $13
         mfc0    $k1, $12        # extract current value of status register $12
         andi    $k1, 0xfffd     # reset status register $12 to enable interrupts
@@ -115,6 +126,5 @@ done:
         eret                    # return control back to program
 
 
-        
-
-
+# end of program
+# end of file
