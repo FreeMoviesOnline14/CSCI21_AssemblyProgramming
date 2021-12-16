edd6773a17139941d5854dac708f564541d198be
Marcellus Von Sacramento <mvps815165@gmail.com>
2021-12-15 22:47:51 -0800

    added comments

--- Final/q6.asm
+++ Final/q6.asm
@@ -16,6 +16,9 @@
 #
 
         .data
+        
+        .align 2
+storage .space 16   # in case we need to save something from kernel text
 extMsg: .asciiz "Exiting program...\n"
 
         .text
@@ -23,11 +26,17 @@ extMsg: .asciiz "Exiting program...\n"
 
 main:   # start of text segment
 
+# enable all interrupts for status register $12 and receiver control register
         li      $s0, 0xffff0000     # base address of I/O
         li      $s1, 2              # 0x000000002 to enable keyboard interrupt
         sw      $s1, 0($s0)         # store 0x000000002 to $s1 to enable interrupt bit of receiver control
         li      $s1, 0xffff         # will be used to enable all exception in status register $12
 
+here:
+        j       here    # stay here until q is pressed, kernel will handle that
+
+        li      $v0, 10     # `service code for returning control to OS
+        syscall
 
 
 
